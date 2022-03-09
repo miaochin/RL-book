8f2d8f12f38356915d60bcb5150e5834ee920b62
Miao-Chin Yen <miaochin.yen@gmail.com>
2022-03-09 03:38:04 -0800

    Finish Assignment 13

---
+++ CME241_assignments/assignment13/glie_mc_func_approx.py
@@ -0,0 +1,90 @@
+from rl.monte_carlo import glie_mc_control
+from rl.markov_decision_process import MarkovDecisionProcess
+from operator import itemgetter
+import itertools
+from typing import Callable, Iterable, Iterator, TypeVar, Set, Sequence, Tuple, Dict, Mapping
+import numpy as np
+from rl.approximate_dynamic_programming import ValueFunctionApprox, \
+    QValueFunctionApprox, NTStateDistribution, extended_vf
+from rl.distribution import Categorical
+from rl.function_approx import LinearFunctionApprox, Weights, Tabular, learning_rate_schedule, DNNSpec
+import rl.iterate as iterate
+import rl.markov_process as mp
+from rl.markov_decision_process import MarkovDecisionProcess
+from rl.markov_decision_process import TransitionStep, NonTerminal
+from rl.monte_carlo import greedy_policy_from_qvf
+from rl.policy import Policy, DeterministicPolicy
+from rl.experience_replay import ExperienceReplayMemory
+from rl.chapter3.simple_inventory_mdp_cap import SimpleInventoryMDPCap, InventoryState
+from rl.distribution import Choose, Gaussian
+from rl.dynamic_programming import evaluate_mrp_result
+from rl.dynamic_programming import policy_iteration_result
+from rl.dynamic_programming import value_iteration_result
+from rl.chapter7.asset_alloc_discrete import AssetAllocDiscrete
+from pprint import pprint
+
+
+
+gamma: float = 0.9
+
+steps: int = 4
+μ: float = 0.13
+σ: float = 0.2
+r: float = 0.07
+a: float = 1.0
+init_wealth: float = 1.0
+init_wealth_stdev: float = 0.1
+excess: float = μ - r
+var: float = σ * σ
+base_alloc: float = excess / (a * var)
+
+risky_ret: Sequence[Gaussian] = [Gaussian(μ=μ, σ=σ) for _ in range(steps)]
+riskless_ret: Sequence[float] = [r for _ in range(steps)]
+utility_function: Callable[[float], float] = lambda x: - np.exp(-a * x) / a
+
+alloc_choices: Sequence[float] = np.linspace(
+    2 / 3 * base_alloc,
+    4 / 3 * base_alloc,
+    11
+)
+
+feature_funcs: Sequence[Callable[[Tuple[float, float]], float]] = \
+    [
+        lambda _: 1.,
+        lambda w_x: w_x[0],
+        lambda w_x: w_x[1],
+        lambda w_x: w_x[1] * w_x[1]
+    ]
+    
+dnn: DNNSpec = DNNSpec(
+    neurons=[],
+    bias=False,
+    hidden_activation=lambda x: x,
+    hidden_activation_deriv=lambda y: np.ones_like(y),
+    output_activation=lambda x: - np.sign(a) * np.exp(-x),
+    output_activation_deriv=lambda y: -y
+)
+
+init_wealth_distr: Gaussian = Gaussian(μ=init_wealth, σ=init_wealth_stdev)
+
+aad: AssetAllocDiscrete = AssetAllocDiscrete(
+    risky_return_distributions=risky_ret,
+    riskless_returns=riskless_ret,
+    utility_func=utility_function,
+    risky_alloc_choices=alloc_choices,
+    feature_functions=feature_funcs,
+    dnn_spec=dnn,
+    initial_wealth_distribution=init_wealth_distr
+)
+
+epsilon_as_func_of_episodes: Callable[[float], float] = lambda k: k ** -0.5
+
+glie_mc_result = glie_mc_control(
+    mdp = aad.get_mdp(steps),
+    states=init_wealth_distr.sample(),
+    approx_0=aad.get_qvf_func_approx() ,
+    γ=gamma,
+    ϵ_as_func_of_episodes = epsilon_as_func_of_episodes,
+    episode_length_tolerance= 1e-6
+)
+
---
+++ CME241_assignments/assignment13/glie_sarsa_tabular.py
@@ -0,0 +1,92 @@
+from rl.td import glie_sarsa
+from rl.markov_decision_process import MarkovDecisionProcess
+from operator import itemgetter
+import itertools
+from typing import Callable, Iterable, Iterator, TypeVar, Set, Sequence, Tuple, Dict, Mapping
+import numpy as np
+from rl.approximate_dynamic_programming import ValueFunctionApprox, \
+    QValueFunctionApprox, NTStateDistribution, extended_vf
+from rl.distribution import Categorical
+from rl.function_approx import LinearFunctionApprox, Weights, Tabular, learning_rate_schedule
+import rl.iterate as iterate
+import rl.markov_process as mp
+from rl.markov_decision_process import MarkovDecisionProcess
+from rl.markov_decision_process import TransitionStep, NonTerminal
+from rl.monte_carlo import greedy_policy_from_qvf
+from rl.policy import Policy, DeterministicPolicy
+from rl.experience_replay import ExperienceReplayMemory
+from rl.chapter3.simple_inventory_mdp_cap import SimpleInventoryMDPCap, InventoryState
+from rl.distribution import Choose
+from rl.dynamic_programming import evaluate_mrp_result
+from rl.dynamic_programming import policy_iteration_result
+from rl.dynamic_programming import value_iteration_result
+from pprint import pprint
+from itertools import islice
+
+capacity: int = 2
+poisson_lambda: float = 1.0
+holding_cost: float = 1.0
+stockout_cost: float = 10.0
+
+si_mdp: SimpleInventoryMDPCap = SimpleInventoryMDPCap(
+    capacity=capacity,
+    poisson_lambda=poisson_lambda,
+    holding_cost=holding_cost,
+    stockout_cost=stockout_cost
+)
+
+gamma: float = 0.9
+mc_episode_length_tol: float = 1e-5
+num_episodes = 10000
+
+epsilon_as_func_of_episodes: Callable[[int], float] = lambda k: k ** -0.5
+q_learning_epsilon: float = 0.2
+
+td_episode_length: int = 100
+initial_learning_rate: float = 0.1
+half_life: float = 10000.0
+exponent: float = 1.0
+
+initial_qvf_dict: Mapping[Tuple[NonTerminal[InventoryState], int], float] = {
+        (s, a): 0. for s in si_mdp.non_terminal_states for a in si_mdp.actions(s)
+    }
+    
+learning_rate_func: Callable[[int], float] = learning_rate_schedule(
+    initial_learning_rate=initial_learning_rate,
+    half_life=half_life,
+    exponent=exponent
+    )   
+
+glie_sarsa_result = glie_sarsa(
+    mdp = si_mdp,
+    states=Choose(si_mdp.non_terminal_states),
+    approx_0=Tabular(
+        values_map=initial_qvf_dict,
+        count_to_weight_func=learning_rate_func
+    ),
+    γ=gamma,
+    ϵ_as_func_of_episodes=epsilon_as_func_of_episodes,
+    max_episode_length =td_episode_length
+)
+
+
+
+
+print("MDP Policy Iteration Optimal Value Function and Optimal Policy for SimpleInventoryMDPCap")
+print("--------------")
+opt_vf_pi, opt_policy_pi = policy_iteration_result(
+    si_mdp,
+    gamma
+)
+pprint(opt_vf_pi)
+print(opt_policy_pi)
+print()
+
+print("MDP Value Iteration Optimal Value Function and Optimal Policy for SimpleInventoryMDPCap")
+print("--------------")
+opt_vf_vi, opt_policy_vi = value_iteration_result(si_mdp, gamma)
+pprint(opt_vf_vi)
+print(opt_policy_vi)
+print()
+
+
